e4e66d37418173e4834a81f676a3941603794057
Afifah hanum <151976202+afifahhnm@users.noreply.github.com>
2025-04-11 21:53:08 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("notes", "root", "", {
+const db = new Sequelize("notes", "afifah", "123456", {
   host: "34.50.72.116",
   dialect: "mysql",
 });
