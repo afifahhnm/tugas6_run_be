c87ef922edee32657d83b86b1a75894caecf8a9b
Afifah hanum <151976202+afifahhnm@users.noreply.github.com>
2025-05-25 11:07:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors({credentials: true, origin: 'http://127.0.0.1:5500'}));
+app.use(cors({credentials: true, origin: 'https://d-04-450714.uc.r.appspot.com/login.html'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(NoteRoute);
